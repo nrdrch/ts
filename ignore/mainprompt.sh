a38b4694daeeec4fbe5766619fbe45cadadb6889
Lukas aka sticky aka MoeJoe <96546774+nrdrch@users.noreply.github.com>
2022-06-23 05:50:18 +0200

    Create mainprompt.sh

---
+++ ignore/mainprompt.sh
@@ -0,0 +1,12 @@
+cat $HOME/ts/necessary.txt
+
+#!/bin/bash
+echo "1) Base"
+echo "2) File System Drivers"
+echo "3) Common Applications"
+echo "4) Configure"
+echo "5) GRUB Utilities"
+echo "6) Quit"
+echo " "
+echo " "
+echo "Options used so far:"
