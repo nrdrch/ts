e05f291464e7dfe93eca9794c54be314327150c4
Lukas aka sticky aka MoeJoe <96546774+nrdrch@users.noreply.github.com>
2022-07-07 04:25:36 +0200

    Update choosesh.sh

--- sh/choosesh.sh
+++ sh/choosesh.sh
@@ -9,19 +9,19 @@ select fav in "${options[@]}"; do
              sudo pacman -S --noconfirm fish
              sudo chsh -s /usr/bin/fish &(whoami)
              echo ">>  Installed fish and set it as default | $(date)" >> ~/ts/ignore/mainprompt.sh
-             exit
              
-            ;;
+             
+             ;;
         "Use Zsh")
              sudo pacman -S --noconfirm zsh
              sudo chsh -s /usr/bin/zsh &(whoami)
              echo ">>  Installed zsh and set it as default | $(date)" >> ~/ts/ignore/mainprompt.sh
-            ;;
+             ;;
         "Use Bash")
              sudo pacman -S --noconfirm --needed bash
              sudo chsh -s /bin/bash &(whoami)
              echo ">>  Installed bash and set it as default | $(date)" >> ~/ts/ignore/mainprompt.sh
-            ;;
+             ;;
 
 
         "Back")
