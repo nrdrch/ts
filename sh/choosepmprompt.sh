5de4bc2b42338a55482214e30fbeb6726c9b6346
Lukas aka sticky aka MoeJoe <96546774+nrdrch@users.noreply.github.com>
2022-07-07 05:45:09 +0200

    Create choosepmprompt.sh

---
+++ sh/choosepmprompt.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+cat $HOME/ts/ignore/necessary.txt
+options=("Snap" "Flatpak" "Pip" "Pacaur" "Yay" "Back")
+echo "1) Snap"
+echo "2) Flatpak"
+echo "3) Pip"
+echo "4) Pacaur"
+echo "5) Yay"
+echo "6) Back"
+echo " "
+echo " "
+echo "Options used so far:"
