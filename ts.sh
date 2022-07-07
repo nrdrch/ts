56556015834b960944ae5429419aa63e1b377cab
Lukas aka sticky aka MoeJoe <96546774+nrdrch@users.noreply.github.com>
2022-07-07 03:59:11 +0200

    Add files via upload

--- ts.sh
+++ ts.sh
@@ -10,16 +10,16 @@ options=("i3-gaps Base"  "Get More Apps" "Quit")
 select fav in "${options[@]}"; do
     case $fav in
         "i3-gaps Base")
-	    cd ~/ts/sh/
+	        cd ~/ts/sh/
             sudo chmod +x base.sh
-	    ./base.sh	    
-   	    cd ~/ts/ignore/
-	    ./mainprompt.sh
+	        ./base.sh	    
+   	        cd ~/ts/ignore/
+	       ./mainprompt.sh
 	    
             ;;
-	"Get More Apps")
+	    "Get More Apps")
 		
-	    ;;
+	        ;;
         "Quit")
           clear
         echo "please Note: A reboot is required for various things to function correctly."
