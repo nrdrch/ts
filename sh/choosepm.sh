65b64f49a358f2a8514f6db7c80a18b501bf5122
Lukas aka sticky aka MoeJoe <96546774+nrdrch@users.noreply.github.com>
2022-07-07 09:09:27 +0200

    Update choosepm.sh

--- sh/choosepm.sh
+++ sh/choosepm.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-
+clear 
+cat $HOME/ts/ignore/necessary.txt
+sleep 0.2s
 PS3='Get Another Package Manager: '
 options=("Snap" "Flatpak" "Pip" "Pacaur" "Yay" "Back")
 select fav in "${options[@]}"; do
