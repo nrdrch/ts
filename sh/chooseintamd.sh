99425799918a45cfea566eaad0618d1adb991646
Lukas aka sticky aka MoeJoe <96546774+nrdrch@users.noreply.github.com>
2022-07-07 09:59:55 +0200

    Create chooseintamd.sh

---
+++ sh/chooseintamd.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+cd ~/Applications/KVM*
+sudo chmod +x *.sh
+clear
+echo "moejoe's mildly useful and totally unbiased:"
+sleep 0.4s
+cat $HOME/ts/ignore/necessary.txt
+sleep 0.2s
+PS3='Are you using an Intel or AMD CPU? '
+options=("Intel CPU"  "AMD CPU")
+select fav in "${options[@]}"; do
+    case $fav in
+      "Intel CPU")
+	    cd ~/Applications/KVM-GPU-Passthrough/
+	    sudo ./grub_setup_intel.sh
+	    
+      
+      
+      clear
+	    
+      ;;
+	"AMD CPU")
+	    cd ~/Applications/KVM-GPU-Passthrough/
+      sudo ./grub_setup_amd.sh
+      
+      
+      
+	    ;;
+      
+        *) echo "invalid option $REPLY";;
+    esac
+done
