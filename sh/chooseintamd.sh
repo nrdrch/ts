2df36f3539baa966e08892273ebafc2790e0d62b
Lukas aka sticky aka MoeJoe <96546774+nrdrch@users.noreply.github.com>
2022-07-07 10:01:02 +0200

    Update chooseintamd.sh

--- sh/chooseintamd.sh
+++ sh/chooseintamd.sh
@@ -10,18 +10,19 @@ PS3='Are you using an Intel or AMD CPU? '
 options=("Intel CPU"  "AMD CPU")
 select fav in "${options[@]}"; do
     case $fav in
-      "Intel CPU")
+        "Intel CPU")
 	    cd ~/Applications/KVM-GPU-Passthrough/
 	    sudo ./grub_setup_intel.sh
 	    
       
       
-      clear
+      	    clear
 	    
-      ;;
-	"AMD CPU")
+            ;;
+        "AMD CPU")
 	    cd ~/Applications/KVM-GPU-Passthrough/
-      sudo ./grub_setup_amd.sh
+            sudo ./grub_setup_amd.sh
+	    clear
       
       
       
