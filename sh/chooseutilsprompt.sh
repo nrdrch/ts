fa5f3dc6a596f024566443eec8f76741b8a00426
Lukas aka sticky aka MoeJoe <96546774+nrdrch@users.noreply.github.com>
2022-07-07 13:12:38 +0200

    Create chooseutilsprompt.sh

---
+++ sh/chooseutilsprompt.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+cat $HOME/ts/ignore/necessary.txt
+options=("Did You Mean?(TUI Spellchecker)" "GRUB Utilities"  "Graphical Utilities" "Back")
+echo "1) Did You Mean?(TUI Spellchecker)"
+echo "2) GRUB Utilities"
+echo "3) Graphical Utilities"
+echo "4) Back"
+echo " "
+echo " "
