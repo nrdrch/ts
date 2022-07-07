c6f253a2eb415a734f0847ee6de9e3f16cffe531
Lukas aka sticky aka MoeJoe <96546774+nrdrch@users.noreply.github.com>
2022-07-07 13:03:24 +0200

    Create choosegutils.sh

---
+++ sh/choosegutils.sh
@@ -0,0 +1,38 @@
+#!/bin/bash 
+clear
+cat $HOME/ts/ignore/necessary.txt
+sleep 0.2s
+PS3='Choose an Option: '
+options=("GIMP" "Kden-Live" "K-SNIP" "Back")
+select fav in "${options[@]}"; do
+    case $fav in
+        "GIMP)")
+             sudo pacman -S --noconfirm
+
+             ;;
+        "Kden-Live")
+             cd ~/Applications
+             git clone https://aur.archlinux.org/kdenlive-git.git
+             cd kdenlive-git/
+             makepkg -si
+
+             ;;
+        "K-SNIP")
+             
+             cd ~/Applications
+             git clone https://aur.archlinux.org/ksnip-git.git
+             cd ksnip-git/
+             makepkg -si
+        
+             
+             ;;
+
+        "")
+             ;;
+        "Back")
+          clear
+                exit
+                ;;
+        *) echo "invalid option $REPLY";;
+    esac
+done
