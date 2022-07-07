94ca94ff19755367d3f83c6db14441b157459484
Lukas aka sticky aka MoeJoe <96546774+nrdrch@users.noreply.github.com>
2022-07-07 06:05:59 +0200

    Create choosegmaprompt.sh

---
+++ sh/choosegmaprompt.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+cat $HOME/ts/ignore/necessary.txt
+echo "1) Package Managers"
+echo "2) The Usual Stuff"
+echo "3) Useful Utilities"
+echo "4) Common Applications"
+echo "5) Back"
+echo " "
+echo " "
