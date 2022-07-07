c75ef0951b7a0656fc65d8a60c74e3c0289481b0
Lukas aka sticky aka MoeJoe <96546774+nrdrch@users.noreply.github.com>
2022-07-07 10:04:34 +0200

    Create choosecaprompt.sh

---
+++ sh/choosecaprompt.sh
@@ -0,0 +1,12 @@
+#!/bin/bash 
+clear
+cat $HOME/ts/ignore/necessary.txt
+sleep 0.2s
+options=("Spotify" "Discord" "Telegram" "Session" "Virt-Manager/QEMU/KVM" "")
+echo "1) Spotify"
+echo "2) Discord"
+echo "3) Telegram"
+echo "4) Session"
+echo "5) Virt-Manager/QEMU/KVM"
+echo " "
+echo " "
