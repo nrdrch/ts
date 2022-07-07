93c0f9bb498d41d5fe42f1a349d76cfb7599e19f
Lukas aka sticky aka MoeJoe <96546774+nrdrch@users.noreply.github.com>
2022-07-07 06:07:20 +0200

    Update choosegma.sh

--- sh/choosegma.sh
+++ sh/choosegma.sh
@@ -7,21 +7,37 @@ select fav in "${options[@]}"; do
         "Package Managers")
              cd ~/ts/sh
              ./choosepm.sh
+             clear
+             cd ~/ts/sh 
+             ./choosegmaprompt.sh
 
              ;;
         "The Usual Stuff")
              sudo pacman -S --noconfirm mpv btop neofetch man-db
              sudo pacman -S --noconfirm ntfs-3g udisks2 btrfs-progs exfatprogs f2fs-tools dosfstools jfsutils cryptsetup lvm2 util-linux nilfs-utils xfsprogs
+             clear
+             cd ~/ts/sh 
+             ./choosegmaprompt.sh
 
              ;;
         "Useful Utilities")
              cd ~/ts/sh
              ./chooseutils.sh
              clear
+             cd ~/ts/sh 
+             ./choosegmaprompt.sh
              
              ;;
              
         "Common Applications")
+        
+        
+
+
+
+             clear
+             cd ~/ts/sh 
+             ./choosegmaprompt.sh
             
              ;;
 
